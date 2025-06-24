9cef71a7b468ffb007ab1801b89ef1f4a01ffba7
Digbijay Nayak <digbijaynayak522@gmail.com>
2025-06-24 18:41:50 +0530

    test the delete recipe endpoint

--- test/recipes.test.js
+++ test/recipes.test.js
@@ -515,4 +515,52 @@ describe('test the recipes API', () => {
         );
     });
 
+    //test delete recipe
+    describe('DELETE/recipes/:id', () => {
+        it('Delete the specified recipe',
+            async () => {
+                const res = await request(app)
+                    .delete(`/recipes/${id}`)
+                    .set('Authorization', `Bearer ${token}`);
+                expect(res.statusCode).toEqual(200);
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        message: 'Recipe successfully deleted'
+                    }),
+                );
+            },
+        );
+
+        it('Fail to delete the specified recipe, invalid token',
+            async () => {
+                const res = await request(app)
+                    .delete(`/recipes/${id}`)
+                    .set('Authorization', `Bearer dnsosuew8w9e729203332`);
+                expect(res.statusCode).toEqual(403);
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        message: 'Unauthorized'
+                    }),
+                );
+            },
+        );
+
+        it('Fail to delete the specified recipe, internal server error',
+            async () => {
+                jest.spyOn(RecipeService, 'fetchByIdAndDelete')
+                    .mockRejectedValueOnce(new Error());
+                const res = await request(app)
+                    .delete(`/recipes/${id}`)
+                    .set('Authorization', `Bearer ${token}`);
+                expect(res.statusCode).toEqual(500);
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        success: false,
+                        message: 'An error occured while deleting recipe'
+                    }),
+                );
+            },
+        );
+    });
 });
